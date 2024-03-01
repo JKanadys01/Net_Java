48e9af4774af232f97db2470c372162e6cf0fb34
JKanadys01 <jakubkanadys001@gmail.com>
2024-03-01 19:44:10 +0100

    Lab0_gotowe

---
+++ Lab0_FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab0_FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz(30);
+            fizzbuzz.Display();
+                
+
+        }
+    }
+}
